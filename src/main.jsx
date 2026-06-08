73372ae5a2bf7f95e215d010001e9c7428b7a1ee
SanketRajpurohit <sankrp5@gmail.com>
2026-06-08 11:12:13 +0530

    added sitemap

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ReactGA from "react-ga4";
+
+if (import.meta.env.PROD) {
+  ReactGA.initialize("G-1DR7QGPDTD");
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
